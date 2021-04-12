3d98e50e80236109ecd915e6727cc57bda1777a9
Catalina Garrido <catalinagarrido@MacBook-Air-de-Catalina.local>
2021-04-12 11:33:10 -0400

    final

--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 import requests
 from datetime import date
 
-def paginacion(data, offset):
+def paginacion(data, offset, persona):
     data_nueva = data
     offset = offset
     while True:
-        URL = 'https://tarea-1-breaking-bad.herokuapp.com/api/characters?limit=10&offset='+str(offset)
+        URL = 'https://tarea-1-breaking-bad.herokuapp.com/api/characters?name='+str(persona)+'&limit=10&offset='+str(offset)
         data = requests.get(URL) 
         data = data.json() 
         if len(data)== 0:
@@ -14,7 +14,7 @@ def paginacion(data, offset):
         else:
             offset+=10
             data_nueva +=data
-            return paginacion(data_nueva, offset)
+            return paginacion(data_nueva, offset, persona)
             
     return data_nueva
 
@@ -27,8 +27,6 @@ def current_date_format(date):
     return messsage
     
 def inicio(request):
-    print("SOLICITANDO INFORMACION DE INTERNET")
-    print("espere....") 
     URL1 = 'https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Better+Call+Saul'
     URL2 = 'https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Breaking+Bad' #configuramos la url
     #solicitamos la información y guardamos la respuesta en data.
@@ -53,7 +51,6 @@ def inicio(request):
     return render(request, "index.html", context)
 
 def episodios(request, serie, temporada):
-   
     nombre_serie = "+".join(serie.split(" "))
     URL = 'https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series='+nombre_serie
     data = requests.get(URL) 
@@ -75,11 +72,9 @@ def episodios(request, serie, temporada):
 
 def datos_episodio(request, serie, episodio):
     id_episodio = str(episodio)
-    print(id_episodio)
     URL = 'https://tarea-1-breaking-bad.herokuapp.com/api/episodes/'+id_episodio
     data = requests.get(URL) 
     data = data.json()
-    print(data)
     for i in range(len(data)): #iteramos sobre data
         element = data[i]
         fecha = element["air_date"][:10]
@@ -111,23 +106,16 @@ def datos_personaje(request, personaje):
     return render(request, "datos_personaje.html", context)
 
 def buscar_personaje(request):
-    print("estoy aca")
     if request.method == "POST":
         persona = request.POST['person']
-        print(persona)
         persona = persona.lower()
-        data = paginacion([], 0)
-        caracteres = []
-        print(data)
-        for personaje in data:
-            if persona in personaje["name"].lower():
-                caracteres.append(personaje)
-        print(caracteres)
+        data = paginacion([], 0, persona)
+        
         context = {
             "serie1": "BETTER CALL SAUL",
             "serie2": "BREAKING BAD",
             "busqueda": persona,
-            "personajes": caracteres
+            "personajes": data
         }
         return render(request, "lista_personajes.html", context)
     else:
